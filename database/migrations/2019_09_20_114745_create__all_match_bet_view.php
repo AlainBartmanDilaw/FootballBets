ccf0a1fca900ba73ddc1e4c71c2205a4edb216be
Alain Léglise <fraternite76-alain@yahoo.fr>
2019-09-20 14:29:41 +0200

    Scripts de création des vues x 2

---
+++ database/migrations/2019_09_20_114745_create__all_match_bet_view.php
@@ -0,0 +1,41 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+// use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateAllMatchBetView extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        DB::statement("CREATE ALGORITHM=UNDEFINED DEFINER=`Alain`@`%` SQL SECURITY DEFINER VIEW `allmatchbet` AS SELECT m.*
+     , u.Id User_Id
+   , ( SELECT b.Score
+	      FROM `bet` b
+		  WHERE b.User_Id = u.id
+		    AND b.MatchEquipe_Idt = m.MatchEquipe_Idt_Dom
+     ) ScoreDomicile
+   , ( SELECT b.Score
+	      FROM `bet` b
+		  WHERE b.User_Id = u.id
+		    AND b.MatchEquipe_Idt = m.MatchEquipe_Idt_Ext
+	  ) ScoreExterieur
+FROM `allmatch` m
+   , `users` u");
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        DB::statement('DROP VIEW IF EXISTS `AllMatchBet`');
+    }
+}
