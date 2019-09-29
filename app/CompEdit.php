e77615883c5fb84b9e3725b508319f4448be864e
Alain Léglise <fraternite76-alain@yahoo.fr>
2019-09-29 23:14:19 +0200

    Suppression commentaires inutiles

--- app/CompEdit.php
+++ app/CompEdit.php
@@ -21,13 +21,11 @@ class CompEdit extends Model
 
     public static function GetIdt($CompetitionCod, $EditionCod)
     {
-//        echo "Recherche : " . $CompetitionCod . "/" . $EditionCod . "\n"    ;
         $values = CompEdit::where([
             ['Competition_Cod', $CompetitionCod],
             ['Edition_Cod', $EditionCod],
         ])->get();
         foreach ($values as $value) {
-//            echo $value->idt . "\n";
             return $value->idt; //
         }
         return -1; // Non trouvé !
